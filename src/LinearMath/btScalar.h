636e1a7bdffa56ab9fe8c702a7bb9858205d3da7
Aleš Mlakar <ales.mlakar@outfit7.com>
2016-09-15 21:51:22 +0200

    FIX: Android alignment

--- src/LinearMath/btScalar.h
+++ src/LinearMath/btScalar.h
@@ -239,13 +239,10 @@ inline int	btGetVersion()
 #else
 
 		#define SIMD_FORCE_INLINE inline
-		///@todo: check out alignment methods for other platforms/compilers
-		///#define ATTRIBUTE_ALIGNED16(a) a __attribute__ ((aligned (16)))
-		///#define ATTRIBUTE_ALIGNED64(a) a __attribute__ ((aligned (64)))
-		///#define ATTRIBUTE_ALIGNED128(a) a __attribute__ ((aligned (128)))
-		#define ATTRIBUTE_ALIGNED16(a) a
-		#define ATTRIBUTE_ALIGNED64(a) a
-		#define ATTRIBUTE_ALIGNED128(a) a
+        #define ATTRIBUTE_ALIGNED16(a) a __attribute__((aligned(16)))
+        #define ATTRIBUTE_ALIGNED64(a) a __attribute__((aligned(64)))
+        #define ATTRIBUTE_ALIGNED128(a) a __attribute__((aligned(128)))
+
 		#ifndef assert
 		#include <assert.h>
 		#endif
